4a23df3a715ea85f52ec6df1e9a682207ef95f0c
Gius993 <giuseppevidili9@gmail.com>
2022-06-07 16:48:17 +0200

    fine m1

--- milestone1/js/sript.js
+++ milestone1/js/sript.js
@@ -2,13 +2,25 @@
 const numbersContainer = document.querySelector('.numbers-container')
 
 for(let i = 0; i <= 1000; i++){
-	//box created
-	const  newBox = `<div class="box">${i}</div>`;
-	//stamp
-	let treeFiveClassName = 'tree';
-	if(i % 3 === 0){
-		 treeFiveClassName = 'tree'
+	if(i % 3 === 0 && i % 5 === 0){
+		console.log('fizzbuzz')
+	} else if(i % 5 === 0) {
+		console.log('Buzz')
+	} else if (i % 3 === 0){
+		console.log('fizz')
+	} else {
+		console.log(i);
 	}
 	
-	numbersContainer.innerHTML += newBox;
+	
+	// //box created
+	// const  newBox = `<div class="box">${i}</div>`;
+	// //stamp
+	// let treeFiveClassName = 'tree';
+	// if(i % 3 === 0){
+	// 	 treeFiveClassName = 'tree'
+//}
+	
+	
+	//numbersContainer.innerHTML += newBox;
 }
