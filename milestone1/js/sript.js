de552a8d3168b3698055e4c0741df27cf04312c3
Gius993 <giuseppevidili9@gmail.com>
2022-06-07 16:16:50 +0200

    scritture

--- milestone1/js/sript.js
+++ milestone1/js/sript.js
@@ -4,7 +4,11 @@ const numbersContainer = document.querySelector('.numbers-container')
 for(let i = 0; i <= 1000; i++){
 	//box created
 	const  newBox = `<div class="box">${i}</div>`;
-	
+	//stamp
+	let treeFiveClassName = 'tree';
+	if(i % 3 === 0){
+		 treeFiveClassName = 'tree'
+	}
 	
 	numbersContainer.innerHTML += newBox;
 }
