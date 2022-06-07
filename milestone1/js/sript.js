0ddbfd021a7683aa53de81982d976d222a58f582
Gius993 <giuseppevidili9@gmail.com>
2022-06-07 16:09:02 +0200

    box generati

--- milestone1/js/sript.js
+++ milestone1/js/sript.js
@@ -1,5 +1,10 @@
 // generate box
+const numbersContainer = document.querySelector('.numbers-container')
+
 for(let i = 0; i <= 1000; i++){
+	//box created
+	const  newBox = `<div class="box">${i}</div>`;
 	
-	console.log(i);
+	
+	numbersContainer.innerHTML += newBox;
 }
